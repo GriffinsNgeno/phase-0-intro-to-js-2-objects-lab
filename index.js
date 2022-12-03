55b49e0504779fa4ee61a8198a6967c38822d11b
GriffinsNgeno <griffins.ngeno@student.moringaschool.com>
2022-12-03 13:49:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
 // Write your solution in this file!
-let employee = {
-    name: 'SamSpeedy',
-    streetAddress: 'Abuja Nigeria',
-};
 
-function updateEmployeeWithKeyAndValue(employee, key, value) {
+let employee = {
+    name: 'Griffins',
+    streetAddress : 'Nairobi',
+};
+function updateEmployeeWithKeyAndValue(employee, key, value){
     const newEmployee = {...employee};
     newEmployee[key] = value;
     return newEmployee;
 }
 
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-    employee[key] = value;
-    return employee;
-    
-};
-
-function deleteFromEmployeeByKey(employee, key, value) {
-    return Object.assign({}, employee, { [key]: value });
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value
+    return employee
 }
 
-function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
-    delete employee[key];
-    return employee;
-};
+function deleteFromEmployeeByKey(employee, key, value){
+    return Object.assign({}, employee, { [key]: value})
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key, value){
+    delete employee[key]
+    return employee
+}
